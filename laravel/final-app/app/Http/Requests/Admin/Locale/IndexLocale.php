9439e95b111a315a330a8f23dd5cb3c350b574b1
jgallegosm <jgallegos@evolutionsoluciones.com>
2021-11-05 12:56:22 -0500

    first commit

---
+++ laravel/final-app/app/Http/Requests/Admin/Locale/IndexLocale.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Admin\Locale;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
+
+class IndexLocale extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return Gate::allows('admin.locale.index');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'orderBy' => 'in:id,nombre,otro|nullable',
+            'orderDirection' => 'in:asc,desc|nullable',
+            'search' => 'string|nullable',
+            'page' => 'integer|nullable',
+            'per_page' => 'integer|nullable',
+
+        ];
+    }
+}
---
+++ laravel/final-app/resources/js/admin/locale/Listing.js
@@ -0,0 +1,5 @@
+import AppListing from '../app-components/Listing/AppListing';
+
+Vue.component('locale-listing', {
+    mixins: [AppListing]
+});
